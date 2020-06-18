8e29df93c1192c2ef11e2e537deed43ac4eba91a
Kevin Gonzalez <59139884+KevinNGonzalez@users.noreply.github.com>
2020-06-18 10:49:37 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,19 @@
+basic.forever(function () {
+    if (input.buttonIsPressed(Button.A)) {
+        let sprite: game.LedSprite = null
+        sprite.turn(Direction.Right, 45)
+    }
+})
+basic.forever(function () {
+    if (input.buttonIsPressed(Button.B)) {
+    	
+    }
+})
+basic.forever(function () {
+    basic.showIcon(IconNames.Triangle)
+})
+basic.forever(function () {
+    if (input.buttonIsPressed(Button.AB)) {
+    	
+    }
+})
